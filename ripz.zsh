035c6d94d0d713d5f18324b95922ac0c13bac0d9
Jonathan Dahan <jdehan@etsy.com>
2017-03-01 15:42:27 -0500

    only add reverse option on osx

--- ripz.zsh
+++ ripz.zsh
@@ -1,15 +1,16 @@
 #!env zsh
 
 (( $+commands[rg] )) || exit
+[[ `uname` == Darwin ]] && _ripz_tail_options="-r"
 
 function _ripz_find() {
-  echo $(alias | tail -r | rg --max-count 1 --no-line-number ".*='{0,1}${1//\\/}'{0,1}")
+  echo $(alias | tail $_ripz_tail_options | rg --max-count 1 --no-line-number ".*='{0,1}${1//\\/}'{0,1}")
 }
 
 function _ripz_prexec() {
   [[ -n $(alias $@) ]] && return
-  expand=$(alias -m $1 | cut -d"'" -f2)
-  result=$(_ripz_find "$expand" ${@[2,-1]})
+  local expanded=$(alias -m $1 | cut -d'=' -f2 | cut -d"'" -f2)
+  [[ -z $expanded ]] && local result=$(_ripz_find $@) || local result=$(_ripz_find $expanded ${@[2,-1]})
   [[ -n $result ]] && echo ${RIPZ_TEXT:-'Ripz:'} $result | cut -d'=' -f1
 }
 
